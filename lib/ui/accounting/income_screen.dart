7dfc780aa47cd8f6bf4066d40844607c09fbd4d0
superxgh <mr.reggie.duke@gmail.com>
2022-04-30 20:21:02 +0700

    test upload

---
+++ lib/ui/accounting/income_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AccountingIncomeScreen extends StatefulWidget {
+  const AccountingIncomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AccountingIncomeScreen> createState() => _AccountingIncomeScreenState();
+}
+
+class _AccountingIncomeScreenState extends State<AccountingIncomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
